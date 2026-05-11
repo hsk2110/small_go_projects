c60dd7a64553d6b06dbc24a0077b779969c00e5d
Hyeon Su <ga96qig@tum.de>
2026-05-11 15:02:04 +0200

    REST API project done

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # small_go_projects
-few golang projects
+few golang projects:
+- URL-health-checker
+- word-frequency-check
+- simple REST API
--- rest/main.go
+++ rest/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
+	"slices"
+	"strconv"
 )
 
 type Todo struct {
@@ -13,11 +15,98 @@ type Todo struct {
 	Description string `json:"description"`
 }
 
-func main() {
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
+var todos = []Todo{}
+var id int
+
+func handleTodos(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		w.Header().Set("Content-Type", "application/json")
+
+		err := json.NewEncoder(w).Encode(todos)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	case http.MethodPost:
+		todo := &Todo{}
+		err := json.NewDecoder(r.Body).Decode(todo)
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+			return
+		}
+		todo.ID = id
+		w.Header().Set("Content-Type", "application/json")
+		id++
+		todos = append(todos, *todo)
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(todo)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+
+}
+
+func handleTodosDelete(w http.ResponseWriter, r *http.Request) {
+	requestedID, err := strconv.Atoi(r.PathValue("id"))
+
+	if err != nil {
+		http.Error(w, "invalid id", http.StatusBadRequest)
+		return
+	}
+
+	index := slices.IndexFunc(todos, func(t Todo) bool {
+		return t.ID == requestedID
 	})
 
+	if index < 0 {
+		http.Error(w, "not found", http.StatusNotFound)
+		return
+	} else {
+		todos = slices.Delete(todos, index, index+1)
+		w.WriteHeader(http.StatusNoContent)
+	}
+
+}
+
+func handleTodosUpdate(w http.ResponseWriter, r *http.Request) {
+
+	requestedID, err := strconv.Atoi(r.PathValue("id"))
+
+	if err != nil {
+		http.Error(w, "invalid id", http.StatusBadRequest)
+		return
+	}
+
+	index := slices.IndexFunc(todos, func(t Todo) bool {
+		return t.ID == requestedID
+	})
+
+	if index < 0 {
+		http.Error(w, "not found", http.StatusNotFound)
+		return
+	} else {
+		todo := &Todo{}
+
+		err = json.NewDecoder(r.Body).Decode(todo)
+
+		if err != nil {
+			http.Error(w, "Something went wrong", http.StatusBadRequest)
+			return
+		}
+		todo.ID = requestedID
+		todos[index] = *todo
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(todo)
+	}
+
+}
+
+func main() {
+	http.HandleFunc("/todo", handleTodos)
+	http.HandleFunc("DELETE /todo/{id}", handleTodosDelete)
+	http.HandleFunc("PUT /todo/{id}", handleTodosUpdate)
 	log.Fatal(http.ListenAndServe(":8888", nil))
 
 }
